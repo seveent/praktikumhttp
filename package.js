be059afc8c5aebc894bb4b119a28ea3cdb445940
seveent <144870441+seveent@users.noreply.github.com>
2023-11-02 08:56:05 +0700

    package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "express-js",
+  "version": "1.0.0",
+  "description": "learning http",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "nodemon server.js"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "nodemon": "^2.0.19"
+  }
+}
